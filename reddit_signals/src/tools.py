9f9c7f7484de2399d36d78d7476aca9a8fc769bc
Arbin Timilsina <arbin.timilsina@gmail.com>
2023-05-19 16:49:11 -0400

    Working version

--- reddit_signals/src/tools.py
+++ reddit_signals/src/tools.py
@@ -1,15 +1,13 @@
 import os
-import urllib
 
 def get_engine():
     client_config = eval(os.environ["config"])
 
     username = client_config["db_username"]
-    # Special characters have to be URL-encoded
-    password = urllib.parse.quote(client_config["db_password"]),
+    password = client_config["db_password"]
     host = client_config["db_host"]
     port = client_config["db_port"]
     name = client_config["db_name"]
-    engine = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{name}"
+    engine = f"mysql+pymysql://{username}:{password}@{host}:{port}/{name}"
 
     return engine
